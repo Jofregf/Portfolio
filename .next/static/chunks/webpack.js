9d36537f69f51ea1f5ebb897614e8a3f97f33055
Guillermo Jofre <jofregf@gmail.com>
2022-02-01 19:27:44 -0300

    finalizado

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f6287167be195f8c"; }
+/******/ 		__webpack_require__.h = function() { return "b300d86116d098eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
